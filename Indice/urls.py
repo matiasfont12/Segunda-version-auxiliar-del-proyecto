fed594837a9fa22e2d1fd465906cabae404254d7
Matías Font <matiasfont12@gmail.com>
2022-04-26 20:51:02 -0300

    Cambio de estetica de botones y templates que funcionan

--- Indice/urls.py
+++ Indice/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import inicio, mi_plantilla, otra_vista, login, register, editar
+from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", inicio, name= "inicio"),
-    path("plantilla/", mi_plantilla, name= "plantilla"),
-    path("otravista/",otra_vista, name= "otra_vista"), 
-    path("login/", login,name= "login"),
+    path("", views.inicio, name= "inicio"),
+    path("plantilla/", views.mi_plantilla, name= "plantilla"),
+    path("about/", views.about, name= "about"), 
+    path("login/", views.login,name= "login"),
     path("logout/", LogoutView.as_view(template_name="Indice/logout.html"), name="logout"),
-    path("register/", register, name= "register"),
-    path("editar/", editar, name= "editar")
+    path("register/", views.register, name= "register"),
+    path("editar/", views.editar, name= "editar")
 ]
--- Indice/views.py
+++ Indice/views.py
@@ -10,9 +10,10 @@ from django.contrib.auth.decorators import login_required
 def inicio(request):
     return render(request, "Indice/index.html", {})
 
-def otra_vista(request):
-    return HttpResponse(
-"<h1>Su privacidad es importante para nosotros. Para protejer mejor su privacidad proporcionamos este aviso que explica nuestras practicas de informacion en linea y sobre la manera que se recoje y se utiliza su informacion.<h1>")
+
+def about(request):
+    return render(request,"Indice/about.html", {})
+   
 
 def numero_random(request):
     numero = random.randrange(15,180)
